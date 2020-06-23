03cad8864e5d207a334ccc799f9fc9c6d1c9d399
scraggo <scraggo@gmail.com>
2020-06-23 17:15:51 -0500

    Fix date test

--- test/src/date.spec.js
+++ test/src/date.spec.js
@@ -3,17 +3,21 @@ const sinon = require('sinon');
 
 const { getDateTime } = require('../../src/utils');
 
+const randomDate = new Date('Jan 1 2000');
+const convertedDate = 'Saturday, January 1, 2000 0:0:0';
+
 describe('date utils', function() {
   describe('getDateTime', function() {
     it('gets a specific date time', function() {
-      const res = getDateTime(new Date('Jan 1 2000'));
-      expect(res).to.equal('Saturday, January 1, 2000 0:0:0');
+      const res = getDateTime(randomDate);
+      expect(res).to.equal(convertedDate);
     });
 
     it('gets Date.now as default', function() {
-      sinon.stub(Date.prototype, 'constructor').returns(new Date('Jan 1 2000'));
-      const res = getDateTime(new Date());
-      expect(res).to.equal('Saturday, January 1, 2000 0:0:0');
+      const stub = sinon.stub(global, 'Date').returns(randomDate);
+      const res = getDateTime();
+      expect(res).to.equal(convertedDate);
+      stub.restore();
     });
   });
 });
